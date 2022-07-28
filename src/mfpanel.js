49077cda5b6ca2db4ab3847ad5119e472ea83745
Boatyy <bcou94@gmail.com>
2022-07-27 20:26:27 -0700

    created MFD panel layout. button make function working correctly. Styled panel and buttons. need to create MFD container and have SYS populate first page

--- src/mfpanel.js
+++ src/mfpanel.js
@@ -2,6 +2,27 @@
 export default function createMFP(){
     const makediv = document.createElement('div')
     makediv.id = 'mfpanel'
-    makediv.innerText = 'Multi-Function Panel'
     document.body.appendChild(makediv)
+    const MFDdiv = document.createElement('div')
+    MFDdiv.id = 'MFD'
+    MFDdiv.innerHTML = "<h3> MFD <h3>"
+    makediv.appendChild(MFDdiv)
+    const ENGTFR = document.createElement('div')
+    ENGTFR.id = 'ENGTFR'
+    ENGTFR.innerHTML = "<h3> ENG TFR <h3>"
+    makediv.appendChild(ENGTFR)
+    //buttons
+    function makebutton(id, text){
+        let mfdbutton = document.createElement('button')
+        mfdbutton.id = id
+        mfdbutton.innerText = text
+        makediv.appendChild(mfdbutton)
+    }
+
+    makebutton('ENG', 'ENG')
+    makebutton('SYS', 'SYS')
+    makebutton('INFO', 'INFO')
+    makebutton('CR', 'C/R')
+    makebutton('dArrow', '\u2B0C')
 }
+
