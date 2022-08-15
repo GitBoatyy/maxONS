49b11642d5e52fdb581c3530ed561407893516c8
Ben Coulthard <bencoulthard@Bens-MacBook-Air.local>
2022-08-15 15:02:28 -0700

    created display container to house MDS. need to decide if using JS to create new page or toggle invis

--- src/index.js
+++ src/index.js
@@ -11,7 +11,16 @@ const engbutton = document.getElementById('ENG')
 const sysbutton = document.getElementById('SYS')
 const infobutton = document.getElementById('INFO')
 const crbutton = document.getElementById('CR')
+const displaycontainer = document.getElementById('displaycontainer')
+const maintbutton = document.getElementById('maintbutton')
+
 function crinfo(){
     alert('This button is only equiped on aircraft that have Fail Operational autopilots. C/R (Clear/Recal) button is used to recall fault details in the event of a degraded autoland status')
 }
+function systoggle(){
+    displaycontainer.classList.toggle('invisible')
+
+}
+sysbutton.addEventListener('click', systoggle)
 crbutton.addEventListener('click' , crinfo)
+
